3cdcc56361d4bb8ced56856a476e49c17710132f
John Lo <johnclo208@gmail.com>
2020-06-13 04:30:24 +0200

    fix path issue

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import configureStore from "./configureStore";
 import * as serviceWorker from "./serviceWorker";
-import App from "app.js";
+import App from "./app";
 import "index.css";
 
 const store = configureStore();
